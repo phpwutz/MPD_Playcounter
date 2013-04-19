71529d7e3fae5f365f6ebf8f59db4f1469b1f6f2
Lukas Köll <phpwutz@gmail.com>
2013-04-19 23:04:26 +0200

    initial foo

--- playcountListener.php
+++ playcountListener.php
@@ -8,9 +8,23 @@ $debug = false;
 $mpd = new mpd($srv, $port, $pwd, $debug);
 
 function playListener($songFile){
-	echo $songFile . " was played\n";
+	//echo $songFile . " was played\n";
+	$ret = array();
+	exec('python sticker.py get "'.$songFile.'" playcount', $ret);
+	//var_dump($ret);
+	if(sizeof($ret) < 1){
+		//echo "setting to 1";
+		exec('python sticker.py set "'.$songFile.'" playcount 1', $ret);
+	}else{
+		$tmp = explode("=", $ret[0], 2);
+		$currentCount = (int) $tmp[1];
+		//var_dump($currentCount);
+		$newCount = $currentCount + 1;
+		exec('python sticker.py set "'.$songFile.'" playcount '.$newCount, $ret);
+	}
 };
 
 $listener = new mpdEventListener($mpd);
 $listener->bind(MPDEVENTLISTENER_ONSONGCHANGE, "playListener");
 $listener->startListening();
+//playListener("Tomte/Heureka/07 es ist so dass du fehlst.mp3");
